468717d3fae9833b8d4b0b4f488c91703a985f9e
MohitWayde <62068704+MohitWayde@users.noreply.github.com>
2022-03-14 15:37:00 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,10 @@
+// burger = document.querySelector('.burger')
+
+// navbarRight = document.querySelector('.navbar-right')
+
+
+// burger.addEventListener('click',()=>{
+//     navbarRight.classList.toggle('visibility-navbar-resp');
+//     navbarRight.classList.toggle('hide-navbar-resp');
+// })
+console.log("JS running");
